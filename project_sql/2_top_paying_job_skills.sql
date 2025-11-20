3a94e73b052238e2f9a95e455cce52a185f5f04a
rohith-katikala <sairohitkatikila@gmail.com>
2025-11-20 21:15:51 +0530

    query 2 sample

---
+++ project_sql/2_top_paying_job_skills.sql
@@ -0,0 +1,27 @@
+/*
+Question: What skills are required for the top-paying data analyst jobs?
+- Use the top 10 highest-paying Data Analyst jobs from first query
+- Add the specific skills required for these roles
+- Why? It provides a detailed look at which high-paying jobs demand certain skills, 
+    helping job seekers understand which skills to develop that align with top salaries
+*/
+
+
+SELECT 
+    job_id,
+    job_title,
+    job_location,
+    name AS company_name,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+FROM
+    job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short = 'Data Analyst' AND
+    job_location = 'Anywhere' AND
+    salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10;
